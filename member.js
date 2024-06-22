735c7714f4324f409add7ee38d2b27eae7873a25
Irene Ferrando Terradez <81253193+Ifete@users.noreply.github.com>
2024-06-22 08:17:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+  return [
+    { name: 'Joe', skills: ['JavaScript', 'AngularJS'] },
+    { name: 'John', skills: ['JavaScript', 'AngularJS', 'ReactJS'] },
+    { name: 'Jane', skills: ['JavaScript', 'AngularJS', 'ReactJS', 'NodeJS'] }
+  ];
+}
